a723af86c8b4e1e563bc37856ca616d070e5a98f
Rayanne Barros <rayanneb126@gmail.com>
2022-02-17 20:42:32 -0300

    Add o gráfico de barra (Componente Bar)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Content from './components/Content';
+import Bar from './components/Bar';
 
 function App() {
   return (
     <div>
       <Content />
+      <Bar />
     </div>
   );
 }

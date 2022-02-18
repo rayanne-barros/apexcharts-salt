48b759d9f0cd4c133426678fbb64b8bc1cc55990
Rayanne Barros <rayanneb126@gmail.com>
2022-02-17 22:47:56 -0300

    Add gráfico Scatter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Content from './components/Content';
 import Bar from './components/Bar';
+import Scatter from './components/Scatter';
 
 function App() {
   return (
     <div>
       <Content />
       <Bar />
+      <Scatter />
     </div>
   );
 }
